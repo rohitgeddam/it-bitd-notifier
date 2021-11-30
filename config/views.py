d38043f5fd2a996d50989867b190c6728f4ab2df
rohitgeddam <rohitgeddam2018@gmail.com>
2021-11-30 19:46:00 +0530

    add home style

--- config/views.py
+++ config/views.py
@@ -1,6 +1,16 @@
 from django.views.generic.base import TemplateView
+from events.models import Event
+from notices.models import Notice
 
 
 class HomePageView(TemplateView):
 
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context["notices"] = Notice.objects.all()[:5]
+        context["events"] = Event.objects.all()[:5]
+        return context
