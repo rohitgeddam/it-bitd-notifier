b6feef8bab1db8909a41e4e1b6f0564c4f3a4f6d
rohitgeddam <rohitgeddam2018@gmail.com>
2021-11-30 11:45:43 +0530

    email field required in user creation django admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,34 @@
 # Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, StudentProfile
 
-admin.site.register(User, UserAdmin)
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User as U
+
+class EmailRequiredMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(EmailRequiredMixin, self).__init__(*args, **kwargs)
+        # make user email field required
+        self.fields['email'].required = True
+
+
+class MyUserCreationForm(EmailRequiredMixin, UserCreationForm):
+    pass
+
+
+class MyUserChangeForm(EmailRequiredMixin, UserChangeForm):
+    pass
+
+
+class EmailRequiredUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    add_fieldsets = ((None, {
+        'fields': ('username', 'email', 'password1', 'password2'), 
+        'classes': ('wide',)
+    }),)
+
+# admin.site.unregister(U)
+admin.site.register(User, EmailRequiredUserAdmin)
+admin.site.register(StudentProfile)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,26 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 
 
+# USER_TYPE_CHOICES = (
+#       (1, 'student'),
+#       (2, 'teacher'),
+#       (3, 'secretary'),
+#       (4, 'supervisor'),
+#       (5, 'admin'),
+#   )
+
+class StudentProfile(models.Model):
+    """StudentProfile Class"""
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    def __str__(self):
+        return f"{self.user.username} profile"
+    
+
 class User(AbstractUser):
-    pass
+    # user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    def __str__(self):
+        return self.username
