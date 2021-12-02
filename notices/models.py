4c694dcf8bdd26a05dd76dbcf366d39f026cdc55
rohitgeddam <rohitgeddam2018@gmail.com>
2021-12-02 14:25:39 +0530

    add twilio

--- notices/models.py
+++ notices/models.py
@@ -2,6 +2,32 @@ from django.db import models
 from core.models import YearTag
 
 from django.core.mail import send_mass_mail
+from django.conf import settings
+
+# from django.http import HttpResponse
+from twilio.rest import Client
+
+
+def broadcast_sms(self):
+    message_to_broadcast = f"{self.title}" f"{self.content}"
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(
+                to=recipient, from_=settings.TWILIO_NUMBER, body=message_to_broadcast
+            )
+
+
+def custom_send_email(self):
+    recepient_list = ["vrahul200023@gmail.com", "rohitgeddam2018@gmail.com"]
+    message1 = (
+        self.title,
+        self.content,
+        "notifier.it.bitd@gmail.com",
+        recepient_list,
+    )
+    send_mass_mail((message1,), fail_silently=False)
 
 
 class NoticeFile(models.Model):
@@ -25,12 +51,6 @@ class Notice(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
-        recepient_list = (["vrahul200023@gmail.com", "rohitgeddam2018@gmail.com"],)
-        message1 = (
-            self.title,
-            self.content,
-            "notifier.it.bitd@gmail.com",
-            recepient_list,
-        )
-        send_mass_mail((message1,), fail_silently=False)
+        custom_send_email(self)
+        broadcast_sms(self)
         return super().save(*args, **kwargs)
