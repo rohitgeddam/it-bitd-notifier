275404bdb4fb6049d279e60c3a78106543cf68ac
rohitgeddam <rohitgeddam2018@gmail.com>
2021-12-05 01:56:20 +0530

    modify settings to add DEBUG env

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(env("DEBUG"))
 
 ALLOWED_HOSTS = ["*"]
 
